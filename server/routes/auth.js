2ed27e29c7bdcbc4e27c7337093e5d756d859ce5
Anees <aneesdahot2020@gmail.com>
2024-08-10 17:29:07 +0500

    fixes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -32,49 +32,5 @@ authRouter.post("/api/signup", async (req, res) => {
   }
 });
 
-//* Sign In Route
-authRouter.post("/api/signin", async (req, res) => {
-  try {
-    const { email, password } = req.body;
-
-    const user = await User.findOne({ email });
-    if (!user) {
-      return res
-        .status(400)
-        .json({ msg: "User with this email does not exist!" });
-    }
-
-    const isMatch = await bcryptjs.compare(password, user.password);
-    if (!isMatch) {
-      return res.status(400).json({ msg: "Incorrect password." });
-    }
-
-    const token = jwt.sign({ id: user._id }, "passwordKey");
-    res.json({ token, ...user._doc });
-  } catch (e) {
-    res.status(500).json({ error: e.message });
-  }
-});
-
-authRouter.post("/tokenIsValid", async (req, res) => {
-  try {
-    const token = req.header("x-auth-token");
-    if (!token) return res.json(false);
-    const verified = jwt.verify(token, "passwordKey");
-    if (!verified) return res.json(false);
-
-    const user = await User.findById(verified.id);
-    if (!user) return res.json(false);
-    res.json(true);
-  } catch (e) {
-    res.status(500).json({ error: e.message });
-  }
-});
-
-//* get user data
-authRouter.get("/", auth, async (req, res) => {
-  const user = await User.findById(req.user);
-  res.json({ ...user._doc, token: req.token });
-});
 
 module.exports = authRouter;
