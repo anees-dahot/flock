ec3776cd608d92759a7b7cbfa4f7c810c9b0947d
Anees <aneesdahot2020@gmail.com>
2024-08-11 16:54:30 +0500

    updated storage class

--- lib/utils/storage.dart
+++ lib/utils/storage.dart
@@ -1,9 +1,22 @@
 import 'package:shared_preferences/shared_preferences.dart';
 
+import '../models/user.dart';
+
 class Storage {
   Future<void> saveDate(String key, String value) async {
     SharedPreferences pref = await SharedPreferences.getInstance();
     pref.setString(key, value);
+  }
+
+  Future<void> saveUser(UserModel user) async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    pref.setString(
+        'user', user.toJson()); 
+  }
+
+  Future<void> getUser(UserModel user) async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    pref.getString('user');
   }
 
   Future<void> getData(String key) async {
@@ -15,6 +28,4 @@ class Storage {
     SharedPreferences pref = await SharedPreferences.getInstance();
     pref.remove(key);
   }
-
-  
 }
