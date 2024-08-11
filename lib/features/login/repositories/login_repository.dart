265f7c056acf76a686b0691f85bcf42ca91fac68
Anees <aneesdahot2020@gmail.com>
2024-08-11 15:11:23 +0500

    login repo creared

--- lib/features/login/repositories/login_repository.dart
+++ lib/features/login/repositories/login_repository.dart
@@ -1,6 +1,31 @@
-// class LoginRepository{
+import 'dart:convert';
 
-// Future<void> 
+import 'package:http/http.dart' as http;
 
+import '../../../utils/app_urls.dart';
 
-// }
+class LoginRepository {
+  Future<Map<String, dynamic>> login(String email, String password) async {
+    final response = await http.post(
+      Uri.parse('$baseUrl/api/signin'),
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: json.encode({"email": email, "password": password}),
+    );
+
+    if (response.statusCode == 200) {
+      return {
+        'status': 200,
+        'message': 'Account created successfuly',
+        'token': jsonDecode(response.body)['token']
+      };
+    } else if (response.statusCode == 400) {
+      return {'status': 400, 'message': jsonDecode(response.body)['msg']};
+    } else if (response.statusCode == 500) {
+      return {'status': 500, 'message': jsonDecode(response.body)['error']};
+    } else {
+      return {'status': 500, 'message': jsonDecode(response.body)['error']};
+    }
+  }
+}
---
+++ lib/utils/storage.dart
@@ -0,0 +1,13 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Storage {
+  Future<void> saveDate(String key, String value) async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    pref.setString(key, value);
+  }
+
+  Future<void> deleteData(String key) async {
+    SharedPreferences pref = await SharedPreferences.getInstance();
+    pref.remove(key);
+  }
+}
