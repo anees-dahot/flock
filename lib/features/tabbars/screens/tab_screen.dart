8858b6a872a26865f96c5a249be36c25ea81b870
Anees <aneesdahot2020@gmail.com>
2024-08-14 15:18:28 +0500

    added post bar

--- lib/features/tabbars/screens/tab_screen.dart
+++ lib/features/tabbars/screens/tab_screen.dart
@@ -1,6 +1,8 @@
+import 'package:flock/utils/storage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../../feed/screens/feed_screen.dart';
 import '../../search/screens/search_screen.dart';
 
 class TabScreen extends StatefulWidget {
@@ -13,11 +15,22 @@ class TabScreen extends StatefulWidget {
 class _TabScreenState extends State<TabScreen>
     with SingleTickerProviderStateMixin {
   late TabController _tabController;
+  String? profileImage;
 
   @override
   void initState() {
     super.initState();
     _tabController = TabController(length: 4, vsync: this);
+    getUser();
+    Future.delayed(const Duration(milliseconds: 10), () {
+      print(profileImage);
+    });
+  }
+
+  void getUser() async {
+    await Storage().getUserData().then((value) {
+      profileImage = value!.profileImage;
+    });
   }
 
   @override
@@ -28,7 +41,10 @@ class _TabScreenState extends State<TabScreen>
         appBar: AppBar(
           title: Text(
             'FLOCK',
-            style: Theme.of(context).textTheme.headlineLarge,
+            style: Theme.of(context).textTheme.headlineLarge?.copyWith(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                ),
           ),
           actions: [
             IconButton(
@@ -37,16 +53,19 @@ class _TabScreenState extends State<TabScreen>
                     .pushNamed(SearchScreen.routeName, arguments: true);
               },
               icon: const Icon(CupertinoIcons.search),
+              tooltip: 'Search',
             ),
             IconButton(
               onPressed: () {
-                print('bubble cliked');
+                print('bubble clicked');
               },
               icon: const Icon(CupertinoIcons.chat_bubble_text),
+              tooltip: 'Chat',
             ),
           ],
           bottom: TabBar(
             controller: _tabController,
+            indicatorColor: Colors.white,
             tabs: const [
               Tab(
                 icon: Icon(CupertinoIcons.home),
@@ -58,16 +77,15 @@ class _TabScreenState extends State<TabScreen>
                 icon: Icon(CupertinoIcons.profile_circled),
               ),
               Tab(
-                icon: Icon(CupertinoIcons.home),
+                icon: Icon(CupertinoIcons.settings),
               ),
-              
             ],
           ),
         ),
         body: TabBarView(
           controller: _tabController,
           children: const [
-            Center(child: Text('Content for Tab 1')),
+          FeedScreen(),
             Center(child: Text('Content for Tab 2')),
             Center(child: Text('Content for Tab 3')),
             Center(child: Text('Content for Tab 4')),
