07abee1f80dec7356e607034943dafe1adc3fd6d
Anees <aneesdahot2020@gmail.com>
2024-08-22 11:56:35 +0500

    created post schema

---
+++ server/models/posts.js
@@ -0,0 +1,100 @@
+const postSchema = new mongoose.Schema({
+  userPosted: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  postText: {
+    type: String,
+    trim: true,
+  },
+  postReacts: [
+    {
+      icon: {
+        type: String,
+        trim: true,
+      },
+      text: {
+        type: String,
+        trim: true,
+      },
+      userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    },
+  ],
+  postImages: [
+    {
+      url: { type: String, trim: true },
+    },
+  ],
+  postVideos: [
+    {
+      url: { type: String, trim: true },
+    },
+  ],
+  postedAt: {
+    type: Date,
+    default: Date.now,
+  },
+  privacy: {
+    type: String,
+    enum: ["public", "friends", "private"],
+    default: "public",
+  },
+  tags: [
+    {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  ],
+  totalReacts: { type: Number, default: 0 },
+  totalComments: { type: Number, default: 0 },
+  totalShares: { type: Number, default: 0 },
+  comments: [
+    {
+      user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+      },
+      commentText: {
+        type: String,
+        required: true,
+        trim: true,
+      },
+      commentedAt: {
+        type: Date,
+        default: Date.now,
+      },
+      commentsLikes: [
+        {
+          type: mongoose.Schema.Types.ObjectId,
+          ref: "User",
+        },
+      ],
+      replies: [
+        {
+          user: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+          },
+          replyText: {
+            type: String,
+            required: true,
+            trim: true,
+          },
+          repliedAt: {
+            type: Date,
+            default: Date.now,
+          },
+        },
+      ],
+    },
+  ],
+});
+
+const Post = mongoose.model("Post", postSchema);
+module.exports = Post;
