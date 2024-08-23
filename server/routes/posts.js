7c8d0daea88b29a12b5ef1bc8d1bf6b7f4403345
Anees <aneesdahot2020@gmail.com>
2024-08-23 15:18:13 +0500

    create API to add post

---
+++ server/routes/posts.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const postsRouter = express.Router();
+const auth = require("../middlewares/auth");
+const Post = require("../models/posts");
+
+postsRouter.post("/api/posts/add-post", auth, async (req, res) => {
+  try {
+    const userPosted = req.user;
+    const { postText, postImages, postVideos, privacy } = req.body;
+    const newPost = new Post({
+      userPosted,
+      postText,
+      postImages,
+      postVideos,
+      privacy,
+    });
+    await newPost.save();
+    res
+      .status(200)
+      .json({ message: "Post publishes successfully", post: newPost });
+  } catch (e) {
+    res.status(500).json({ error: e.message });
+  }
+});
