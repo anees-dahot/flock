b570dd428b9a3e6a9c8dcc27be15ed613f8f3777
Anees <aneesdahot2020@gmail.com>
2024-09-06 15:40:58 +0500

    created bloc for getPosts

--- lib/features/feed/bloc/feed_bloc.dart
+++ lib/features/feed/bloc/feed_bloc.dart
@@ -1,14 +1,34 @@
+import 'dart:async';
+
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flock/features/feed/repository/feed_repository.dart';
 import 'package:flock/models/post.dart';
 
 part 'feed_event.dart';
 part 'feed_state.dart';
 
 class FeedBloc extends Bloc<FeedEvent, FeedState> {
-  FeedBloc() : super(FeedInitial()) {
-    on<FeedEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  FeedRepository feedRepository;
+  FeedBloc({required this.feedRepository}) : super(FeedInitial()) {
+    on<GetPostsEvent>(getPosts);
+  }
+
+  FutureOr<void> getPosts(GetPostsEvent event, Emitter<FeedState> emit) async {
+    emit(GetPostsLoadingState());
+    try {
+      final response = await feedRepository.getPosts();
+      if (response['status'] == 200) {
+        emit(GetPostsSuccessState(posts: response['data']));
+      } else if (response['status'] == 400) {
+        emit(GetPostsErrorgState(error: response['message']));
+      } else if (response['status'] == 500) {
+        emit(GetPostsErrorgState(error: response['message']));
+      } else {
+        emit(GetPostsErrorgState(error: response['message']));
+      }
+    } catch (e) {
+      emit(GetPostsErrorgState(error: e.toString()));
+    }
   }
 }
